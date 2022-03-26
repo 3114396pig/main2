32c0ba89fed24b324726b8918ddc565b72e48075
3114396pig <88887515+3114396pig@users.noreply.github.com>
2022-03-26 16:41:07 +0800

    1

---
+++ js/1.js
@@ -0,0 +1,95 @@
+let myImage = document.querySelector('.img');
+
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage2 = document.querySelector('.img2');
+
+
+myImage2.onclick = function() {
+    let mySrc = myImage2.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage2.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage2.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage3 = document.querySelector('.img3');
+
+
+myImage3.onclick = function() {
+    let mySrc = myImage3.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage3.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage3.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage4 = document.querySelector('.img4');
+
+
+myImage4.onclick = function() {
+    let mySrc = myImage4.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage4.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage4.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage5 = document.querySelector('.img5');
+
+
+myImage5.onclick = function() {
+    let mySrc = myImage5.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage5.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage5.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage6 = document.querySelector('.img6');
+
+
+myImage6.onclick = function() {
+    let mySrc = myImage6.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage6.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage6.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage7 = document.querySelector('.img7');
+
+
+myImage7.onclick = function() {
+    let mySrc = myImage7.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage7.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage7.setAttribute('src', 'img/pig.png');
+    }
+}
+
+let myImage8 = document.querySelector('.img8');
+
+
+myImage8.onclick = function() {
+    let mySrc = myImage8.getAttribute('src');
+    if(mySrc === 'img/pig.png') {
+      myImage8.setAttribute('src', 'img/dog.png');
+    } else {
+      myImage8.setAttribute('src', 'img/pig.png');
+    }
+}
